51d29bc8a2607823434592fd856362081ad4c732
Joshua Falk <joshua.falk@uni-oldenburg.de>
2025-03-13 16:29:00 +0100

    Tooltips config

--- src/main/java/org/example/luxusrechnerjava/ConfigViewController.java
+++ src/main/java/org/example/luxusrechnerjava/ConfigViewController.java
@@ -1,8 +1,12 @@
 package org.example.luxusrechnerjava;
 
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.util.Duration;
 
 public class ConfigViewController extends SubViewController {
     public Button budgetInfoButton;
@@ -20,6 +24,43 @@ public class ConfigViewController extends SubViewController {
     public Button partWeekInfoButton;
     public Button partWeekParBudgetButton;
     public Button partWeekFullBudgetButton;
+    private static final String budgetInfo = "Wie hoch ist das Budget pro Woche in ganzen Euro";
+    private static final String cycleLengthInfo = "Wie viele Tage vergehen gewöhnlich zwischen Zahlungen";
+    private static final String weekFormatInfo = "Wochen können von Montag bis Sonntag oder in 7 Tage Abschnitten ab Zahlungeingang berechnet werden";
+    private static final String saveExpensesInfo = "Sollen die Ausgaben der aktuellen Woche gespeichert werden?";
+    private static final String partBudgetInfo = "Wenn der Zeitraum nicht volle Wochen enthält, werden diese als ganze Wochen bertachtet oder das Budget entsprechend der Tage verringert";
+    private int budget;
+    private int cycleLength;
+
+    private void addTooltip(Control target, String text) {
+        Tooltip tooltip = new Tooltip(text);
+        tooltip.setShowDelay(new Duration(0));
+        target.setTooltip(tooltip);
+    }
+
+    private void setBudgetOutput(int budget) {
+        budgetInputField.setPromptText("Aktuell: " + budget + " €");
+    }
+
+    private void setCycleOutput(int days) {
+        resetCycleInputField.setPromptText("Aktuell: " + days + " Tage");
+    }
+
+    public void initialize() {
+        //add tooltips to info buttons
+        addTooltip(budgetInfoButton, budgetInfo);
+        addTooltip(resetCycleInfoButton, cycleLengthInfo);
+        addTooltip(weekFormatInfoButton, weekFormatInfo);
+        addTooltip(saveExpensesInfoButton, saveExpensesInfo);
+        addTooltip(partWeekInfoButton, partBudgetInfo);
+        //output current values to input fields
+        //TODO read current values for budget and cycleLength
+        budget = (int)(Math.random() * 20 + 90); //placeholder
+        cycleLength = (int)(Math.random() * 5 + 28); //placeholder
+        setBudgetOutput(budget);
+        setCycleOutput(cycleLength);
+
+    }
 
     public void onClickBudgetConfirmButton(ActionEvent actionEvent) {
 
