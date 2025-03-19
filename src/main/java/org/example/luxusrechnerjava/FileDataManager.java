1584b8daed57044a8e96c2f214a8cb4749c081ed
Joshua Falk <joshua.falk@uni-oldenburg.de>
2025-03-19 18:01:28 +0100

    dynamically creates config.properties in execution directory if not found

--- src/main/java/org/example/luxusrechnerjava/FileDataManager.java
+++ src/main/java/org/example/luxusrechnerjava/FileDataManager.java
@@ -3,62 +3,85 @@ package org.example.luxusrechnerjava;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.Properties;
 
-public class FileDataManager extends DataManager{
+public class FileDataManager extends DataManager {
     private final Properties config;
-    private final FileWriter configWriter;
+    private final FileWriter configFileWriter;
+    private final String RESET_DATE_NAME = "resetDate";
+    private final String SAVED_EXPENSES_NAME = "savedExpenses";
+    private final String BUDGET_NAME = "budget";
+    private final String CYCLE_LENGTH_NAME = "cycleLength";
+    private final String WEEK_FORMAT_NAME = "weekFormat";
+    private final String SAVE_EXPENSES_CONFIG_NAME = "saveExpensesConfig";
+    private final String PART_BUDGET_NAME = "partBudget";
 
     /**
      * initializes with default values
      */
-    FileDataManager(String configFilePath) throws IOException {
+    FileDataManager(Path configFilePath) throws IOException {
         config = new Properties();
-        configWriter = new FileWriter(configFilePath);
-        config.load(new FileInputStream(configFilePath));
+        if (!Files.exists(configFilePath)) {
+            //config file does not exist then create file with default values
+            Files.createFile(configFilePath);
+            config.setProperty(RESET_DATE_NAME, LocalDate.now().toString());
+            config.setProperty(SAVED_EXPENSES_NAME, String.valueOf(1245));
+            config.setProperty(BUDGET_NAME, String.valueOf(10000));
+            config.setProperty(CYCLE_LENGTH_NAME, String.valueOf(30));
+            config.setProperty(WEEK_FORMAT_NAME, WeekFormat.MO_TO_SO.toString());
+            config.setProperty(SAVE_EXPENSES_CONFIG_NAME, String.valueOf(true));
+            config.setProperty(PART_BUDGET_NAME, String.valueOf(false));
+            configFileWriter = new FileWriter(configFilePath.toString());
+            config.store(configFileWriter, null);
+        } else {
+            config.load(new FileInputStream(configFilePath.toString()));
+            configFileWriter = new FileWriter(configFilePath.toString());
+        }
     }
 
     @Override
     LocalDate getResetDate() {
-        return LocalDate.parse(config.getProperty("resetDate"));
+        return LocalDate.parse(config.getProperty(RESET_DATE_NAME));
     }
 
     @Override
     int getSavedExpenses() {
-        return Integer.parseInt(config.getProperty("savedExpenses"));
+        return Integer.parseInt(config.getProperty(SAVED_EXPENSES_NAME));
     }
 
     @Override
     int getBudgetConfig() {
-        return Integer.parseInt(config.getProperty("budget"));
+        return Integer.parseInt(config.getProperty(BUDGET_NAME));
     }
 
     @Override
     int getCycleLengthConfig() {
-        return Integer.parseInt(config.getProperty("cycleLength"));
+        return Integer.parseInt(config.getProperty(CYCLE_LENGTH_NAME));
     }
 
     @Override
     WeekFormat getWeekFormatConfig() {
-        return WeekFormat.valueOf(config.getProperty("weekFormat"));
+        return WeekFormat.valueOf(config.getProperty(WEEK_FORMAT_NAME));
     }
 
     @Override
     boolean getSaveExpensesConfig() {
-        return Boolean.parseBoolean(config.getProperty("saveExpenses"));
+        return Boolean.parseBoolean(config.getProperty(SAVE_EXPENSES_CONFIG_NAME));
     }
 
     @Override
     boolean getPartBudgetConfig() {
-        return Boolean.parseBoolean(config.getProperty("partBudget"));
+        return Boolean.parseBoolean(config.getProperty(PART_BUDGET_NAME));
     }
 
     @Override
     void setResetDate(LocalDate resetDate) {
         try {
-            config.setProperty("resetDate", resetDate.toString());
-            config.store(configWriter, null);
+            config.setProperty(RESET_DATE_NAME, resetDate.toString());
+            config.store(configFileWriter, null);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -67,8 +90,8 @@ public class FileDataManager extends DataManager{
     @Override
     void setSavedExpenses(int expenses) {
         try {
-            config.setProperty("savedExpenses", String.valueOf(expenses));
-            config.store(configWriter, null);
+            config.setProperty(SAVED_EXPENSES_NAME, String.valueOf(expenses));
+            config.store(configFileWriter, null);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -77,8 +100,8 @@ public class FileDataManager extends DataManager{
     @Override
     void setBudgetConfig(int budget) {
         try {
-            config.setProperty("budget", String.valueOf(budget));
-            config.store(configWriter, null);
+            config.setProperty(BUDGET_NAME, String.valueOf(budget));
+            config.store(configFileWriter, null);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -87,8 +110,8 @@ public class FileDataManager extends DataManager{
     @Override
     void setCycleLengthConfig(int days) {
         try {
-            config.setProperty("cycleLength", String.valueOf(days));
-            config.store(configWriter, null);
+            config.setProperty(CYCLE_LENGTH_NAME, String.valueOf(days));
+            config.store(configFileWriter, null);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -97,8 +120,8 @@ public class FileDataManager extends DataManager{
     @Override
     void setWeekFormatConfig(WeekFormat format) {
         try {
-            config.setProperty("weekFormat", format.toString());
-            config.store(configWriter, null);
+            config.setProperty(WEEK_FORMAT_NAME, format.toString());
+            config.store(configFileWriter, null);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -107,8 +130,8 @@ public class FileDataManager extends DataManager{
     @Override
     void setSaveExpensesConfig(boolean isSaveExpenses) {
         try {
-            config.setProperty("saveExpenses", String.valueOf(isSaveExpenses));
-            config.store(configWriter, null);
+            config.setProperty(SAVE_EXPENSES_CONFIG_NAME, String.valueOf(isSaveExpenses));
+            config.store(configFileWriter, null);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -117,8 +140,8 @@ public class FileDataManager extends DataManager{
     @Override
     void setPartBudgetConfig(boolean isPartBudget) {
         try {
-            config.setProperty("partBudget", String.valueOf(isPartBudget));
-            config.store(configWriter, null);
+            config.setProperty(PART_BUDGET_NAME, String.valueOf(isPartBudget));
+            config.store(configFileWriter, null);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/luxusrechnerjava/Main.java
+++ src/main/java/org/example/luxusrechnerjava/Main.java
@@ -6,13 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class Main extends Application {
     static DataManager dataManager;
 
     @Override
     public void start(Stage stage) throws IOException {
-        dataManager = new FileDataManager(Main.class.getResource("config.properties").getPath());
+        dataManager = new FileDataManager(Paths.get("config.properties"));
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle(IOHandler.MAIN_TITLE);
